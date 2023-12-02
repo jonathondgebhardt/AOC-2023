2fed284f264c72a3ed723bcb078cd8f78e4153e3
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2023-12-01 20:38:46 -0500

    Simplify solution for day 1.

--- solutions/1/1.cpp
+++ solutions/1/1.cpp
@@ -8,125 +8,114 @@
 
 namespace
 {
+    constexpr std::array<std::pair<std::string, int64_t>, 9> Numbers = {
+        std::make_pair("one", 1), std::make_pair("two", 2),   std::make_pair("three", 3), std::make_pair("four", 4), std::make_pair("five", 5),
+        std::make_pair("six", 6), std::make_pair("seven", 7), std::make_pair("eight", 8), std::make_pair("nine", 9)};
+
+
+    std::vector<std::pair<size_t, int64_t>> GetNumbers(const std::string& x)
+    {
+        std::vector<std::pair<size_t, int64_t>> numbers;
+
+        for(auto i = 0; i < x.size(); ++i)
+        {
+            if(std::isdigit(static_cast<unsigned char>(x[i])) != 0)
+            {
+                const auto number = util::StringTo<int64_t>(std::string(1, static_cast<char>(x[i])));
+                numbers.emplace_back(i, number);
+            }
+        }
+
+        return numbers;
+    }
+
+    std::vector<std::pair<size_t, int64_t>> GetTextNumbers(const std::string& x)
+    {
+        std::vector<std::pair<size_t, int64_t>> numbers;
+
+        for(auto i = 0; i < x.size(); ++i)
+        {
+            for(const auto& [text, value] : Numbers)
+            {
+                if(x.substr(i, text.size()) == text)
+                {
+                    numbers.emplace_back(i, value);
+                    break;
+                }
+            }
+        }
+
+        return numbers;
+    }
+
     namespace part1
     {
-        int64_t GetFirst(const std::string& x)
-        {
-            // TODO: Replace with std::find
-            for(unsigned char c : x)
-            {
-                if(std::isdigit(c) != 0)
-                {
-                    return util::StringTo<int64_t>(std::string(1, static_cast<char>(c)));
-                }
-            }
-
-            return {};
-        }
-
-        int64_t GetLast(const std::string& x)
-        {
-            // TODO: Replace with std::find
-            for(unsigned char c : std::ranges::reverse_view(x))
-            {
-                if(std::isdigit(c) != 0)
-                {
-                    return util::StringTo<int64_t>(std::string(1, static_cast<char>(c)));
-                }
-            }
-
-            return {};
-        }
-
         int64_t GetCalibrationValue(const std::string& x)
         {
-            return GetFirst(x) * 10 + GetLast(x);
-        }
+            const auto numbers = ::GetNumbers(x);
 
+            const auto tens = [&]() -> int64_t {
+                if(!numbers.empty())
+                {
+                    return numbers.front().second * 10;
+                }
+
+                return 0;
+            }();
+
+            const auto ones = [&]() -> int64_t {
+                if(numbers.size() == 1)
+                {
+                    return numbers.front().second;
+                }
+                else if(numbers.size() >= 2)
+                {
+                    return numbers.back().second;
+                }
+
+                return 0;
+            }();
+
+            return tens + ones;
+        }
     }
 
     namespace part2
     {
-        constexpr std::array<std::pair<const char*, int64_t>, 9> Numbers = {
-            std::make_pair("one", 1), std::make_pair("two", 2),   std::make_pair("three", 3), std::make_pair("four", 4), std::make_pair("five", 5),
-                                                        std::make_pair("six", 6), std::make_pair("seven", 7), std::make_pair("eight", 8), std::make_pair("nine", 9)};
-        using Position = std::pair<std::optional<int64_t>, size_t>;
-
-        int64_t GetFirst(const std::string& x)
-        {
-            const auto literal = [&x]() -> Position {
-                for(auto it = x.begin(); it != x.end(); ++it)
-                {
-                    if(std::isdigit(*it) != 0)
-                    {
-                        const auto number = util::StringTo<int64_t>(std::string(1, static_cast<char>(*it)));
-                        const auto index = std::distance(x.begin(), it);
-                        return {number, index};
-                    }
-                }
-
-                return {};
-            }();
-
-            const auto text = [&x]() -> Position {
-                Position p = {std::nullopt, x.size()};
-                for(const auto& [text, num] : Numbers) {
-                    if(const auto pos = x.find(text); pos != std::string::npos && pos < p.second)
-                    {
-                        p = {num, pos};
-                    }
-                }
-
-                return p;
-            }();
-
-            if(literal.second < text.second && literal.first.has_value())
-            {
-                return literal.first.value();
-            }
-
-            return text.first.value_or(int64_t{});
-        }
-
-        int64_t GetLast(const std::string& x)
-        {
-            const auto literal = [&x]() -> Position {
-                for(auto it = x.rbegin(); it != x.rend(); ++it)
-                {
-                    if(std::isdigit(*it) != 0)
-                    {
-                        const auto number = util::StringTo<int64_t>(std::string(1, static_cast<char>(*it)));
-                        const auto index = std::distance(x.rbegin(), it);
-                        return {number, index};
-                    }
-                }
-
-                return {};
-            }();
-
-            const auto text = [&x]() -> Position {
-                Position p = {std::nullopt, x.size()};
-                for(const auto& [text, num] : Numbers) {
-                    if(const auto pos = x.rfind(text); pos != std::string::npos &&  x.size() - pos < p.second)
-                    {
-                        p = {num, x.size() - pos};
-                    }
-                }
-
-                return p;
-            }();
-
-            if(literal.second < text.second && literal.first.has_value())
-            {
-                return literal.first.value();
-            }
-
-            return text.first.value_or(int64_t{});
-        }
-
         int64_t GetCalibrationValue(const std::string& x)
         {
-            return GetFirst(x) * 10 + GetLast(x);
+            auto numbers = ::GetNumbers(x);
+            const auto textNumbers = ::GetTextNumbers(x);
+            std::copy(textNumbers.begin(), textNumbers.end(), std::back_inserter(numbers));
+            std::sort(numbers.begin(), numbers.end(), [](const std::pair<size_t, int64_t> first, const std::pair<size_t, int64_t> second){
+                  return first.first < second.first;
+            });
+
+            const auto tens = [&]() -> int64_t
+            {
+                if(!numbers.empty())
+                {
+                    return numbers.front().second * 10;
+                }
+
+                return 0;
+            }();
+
+            const auto ones = [&]() -> int64_t
+            {
+                if(numbers.size() == 1)
+                {
+                    return numbers.front().second;
+                }
+                else if(numbers.size() >= 2)
+                {
+                    return numbers.back().second;
+                }
+
+                return 0;
+            }();
+
+            return tens + ones;
         }
     }
 
